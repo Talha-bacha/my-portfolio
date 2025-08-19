b7623fb4a54c2ffba63449e261af10ff48e7474b
Muhammad Talha Kaka Khel <muhammadtalhakakakhel96@gmail.com>
2025-08-19 01:04:44 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Open WhatsApp Chat
+function openWhatsApp() {
+  let phoneNumber = "923447742131"; // apna number country code ke sath
+  window.open("https://wa.me/" + phoneNumber, "_blank");
+}
